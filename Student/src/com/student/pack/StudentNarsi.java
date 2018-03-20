3b4e2bcdef6352dd4182decf3da20ee62a464334
Sravanthi Nimmagadda <Sravanthi Nimmagadda@192.168.2.4>
2018-03-20 22:10:18 +0530

    Student initial commit

---
+++ Student/src/com/student/pack/StudentNarsi.java
@@ -0,0 +1,28 @@
+package com.student.pack;
+
+public class StudentNarsi {
+	
+	private int sId;
+	private String name;
+	private String sclass;
+	public int getsId() {
+		return sId;
+	}
+	public void setsId(int sId) {
+		this.sId = sId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSclass() {
+		return sclass;
+	}
+	public void setSclass(String sclass) {
+		this.sclass = sclass;
+	}
+	
+
+}
---
+++ Student/src/com/student/pack/Test.java
@@ -0,0 +1,16 @@
+package com.student.pack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentNarsi student = new StudentNarsi();
+		student.setsId(1);
+		student.setName("Narsi");
+		student.setSclass("Inter");
+		
+		System.out.println(student.getsId() + student.getName() + student.getSclass());
+
+	}
+
+}
